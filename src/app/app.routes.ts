92c287ab82c0c5141d18e5c7d70849edd2f8bc31
Garikoitz <gbereciartua@galde.app>
2024-05-30 15:34:01 +0200

    crear ficheros listado y routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listado',
+    pathMatch: 'full'
+  },
+  {
+    path: 'listado',
+    component: ListadoComponent,
+  }
+];
