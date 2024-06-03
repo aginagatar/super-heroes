0b5395adc4a530fcbcdf1bf9460f122cf06e3735
Garikoitz <gbereciartua@galde.app>
2024-06-03 12:15:00 +0200

    empezando form básico

--- src/app/demo/pages/form-elements/basic-elements/basic-elements.component.ts
+++ src/app/demo/pages/form-elements/basic-elements/basic-elements.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-elements',
@@ -9,4 +10,31 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './basic-elements.component.html',
   styleUrls: ['./basic-elements.component.scss'],
 })
-export default class BasicElementsComponent {}
+export default class BasicElementsComponent {
+
+  formulario: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      nombre: ['', Validators.required]
+    });
+  }
+
+  enviar() {
+    if (this.formulario.valid) {
+      // Realizar acción de envío aquí
+      console.log('Formulario válido. Datos:', this.formulario.value);
+    } else {
+      // Marcar los campos inválidos como tocados para que se muestren los mensajes de error
+      this.marcarCamposComoTocados();
+    }
+  }
+
+  marcarCamposComoTocados() {
+    for (const control in this.formulario.controls) {
+      if (this.formulario.controls.hasOwnProperty(control)) {
+        this.formulario.controls[control].markAsTouched();
+      }
+    }
+  }
+}
