9c4acb01e3b86ffa3013b0150ab03b7febb24e76
Garikoitz <gbereciartua@galde.app>
2024-06-04 12:49:59 +0200

    .

--- src/app/demo/services/super-heroes.service.ts
+++ src/app/demo/services/super-heroes.service.ts
@@ -9,7 +9,7 @@ import { SuperHeroe } from '../model/superHeroe.model';
 })
 export class SuperHeroesService {
 
-  private endpoint = 'https://ca62a382e0f1c0c5062ba.free.beeceptor.com/api/';
+  private endpoint = 'https://ca62a382e0f1c0c5062b.free.beeceptor.com/api/';
 
   constructor(public http: HttpClient) {}
 
